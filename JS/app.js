0894ee7732529b6500c01a2aead97fb440d5e255
vpatel07 <vpatel077375@gmail.com>
2020-10-04 11:07:48 -0400

    OH YEA

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
+(function () {
+'use strict';
 
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function ($scope) {
+  $scope.name = "";
+  $scope.totalvalue = 0;
+
+});
+
+})();
