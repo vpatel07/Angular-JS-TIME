4dad84389641b60ae8637648609a2fe4b3a90904
vpatel07 <vpatel077375@gmail.com>
2020-10-29 15:12:08 -0400

    imajustdabnow:)

--- JS/app2.js
+++ JS/app2.js
@@ -1,16 +1,52 @@
+// (function () {
+// 'use strict';
+//
+// var shoppingList = [
+//   "Milk", "Food", "even more food", "Chocolate", "Candy", "Even more Candy", "The BEST Candy IN THE WORLD"
+// ];
+//
+// angular.module('ShoppingListApp', [])
+// .controller('ShoppingListController', ShoppingListController);
+//
+// ShoppingListController.$inject = ['$scope'];
+// function ShoppingListController($scope) {
+//   $scope.shoppingList = shoppingList;
+// }
+//
+// })();
+
 (function () {
 'use strict';
 
-var shoppingList = [
-  "Milk", "Food", "even more food", "Chocolate", "Candy", "Even more Candy", "The BEST Candy IN THE WORLD"
-];
+angular.module('ControllerAsApp', [])
+.controller('ParentController1', ParentController1)
+.controller('ChildController1', ChildController1)
+.controller('ParentController2', ParentController2)
+.controller('ChildController2', ChildController2);
 
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+ParentController1.$inject = ['$scope'];
+function ParentController1($scope) {
+  $scope.parentValue = 1;
+  $scope.pc = this;
+  $scope.pc.parentValue = 1;
+}
 
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList = shoppingList;
+ChildController1.$inject = ['$scope'];
+function ChildController1($scope) {
+  console.log("$scope.parentValue: ", $scope.parentValue);
+  console.log("CHILD $scope: ", $scope);
+
+  $scope.parentValue = 5;
+  console.log("*** CHANGED: $scope.parentValue = 5 ***");
+  console.log("$scope.parentValue: ", $scope.parentValue);
+  console.log($scope);
+}
+
+// Controller As Syntax
+ParentController2.$inject = ['$scope'];
+function ParentController2($scope) {
+  var parent = this;
+  parent.value = 1;
 }
 
 })();
--- JS/prototype.js
+++ JS/prototype.js
@@ -1,18 +1,48 @@
 // Prototypal inheritance
-var parent = {
-  value "parentValue",
-  obj: {
-    objValue: "parentObjValue"
-  },
-  walk: function () {
-    console.log("walking!");
-  }
-};
+// var parent = {
+//   value: "parentValue",
+//   obj: {
+//     objValue: "parentObjValue"
+//   },
+//   walk: function () {
+//     console.log("walking!");
+//   }
+// };
+//
+// var child = Object.create(parent);
+// console.log("CHILD - child.value: ", child.value);
+// console.log("CHILD - child.obj.objValue", child.obj.objValue);
+// console.log("PARENT - parent.value: ", parent.value);
+// console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
+// console.log("parent: ", parent);
+// console.log("child: ", child);
+//
+// child.value = "childObjValue";
+// child.objValue = "childObjValue";
+// console.log("*** CHANGED: child.value = 'childObjValue'");
+// console.log("*** CHANGED: child.obj.objValue = 'childObjValue'");
+// console.log("CHILD - child.value: ", child.value);
+// console.log("CHILD - child.obj.objValue", child.obj.objValue);
+// console.log("PARENT - parent.value", parent.value);
+// console.log("PARENT - parent.obj.objValue", parent.obj.objValue);
+// console.log("parent: ", parent);
+// console.log("child: ", child);
+//
+// console.log("child.obj === parent.obj ? ", child.obj === parent.obj);
+//
+// var grandChild = Object.create(child);
+// console.log("Grandchild: ", grandChild);
+// grandChild.walk();
 
-var child = Object.create(parent);
-console.log("CHILD - child.value: ", child.value);
-console.log("CHILD - child.obj.objValue", child.obj.objValue);
-console.log("PARENT - parent.value: ", parent.value);
-console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
-console.log("parent: ", parent);
-console.log("child: ", child);
+// function constuctions
+
+// function Dog(name) {
+//   this.name = name;
+//   console.log("'this' is: ", this);
+// }
+// 
+// var myDog = new Dog("Max");
+// console.log("myDog: ", myDog);
+//
+// // NO NO function construcitoner
+// Dog("Max2");
